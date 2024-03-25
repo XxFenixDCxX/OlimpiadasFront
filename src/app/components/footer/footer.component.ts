ff44ab4c5170a4634fbc91657ed07bff24b6a95e
Javier <javierpe12345@gmail.com>
2024-03-25 12:42:28 +0100

    Footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { IonToolbar, IonicModule } from '@ionic/angular';
+import { IonFooter } from "@ionic/angular/standalone";
 
 @Component({
   standalone: true,
   selector: 'app-footer',
   templateUrl: './footer.component.html',
+  imports: [IonicModule],
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent  implements OnInit {
