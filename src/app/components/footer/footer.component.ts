a36f450461a131600e2d75d87a03f9a30a1d134c
AMIN <aminbensbaho@gmail.com>
2024-03-25 12:47:58 +0100

    Update footer component and logo file

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { IonToolbar, IonicModule } from '@ionic/angular';
-import { IonFooter } from "@ionic/angular/standalone";
 
 @Component({
   standalone: true,
   imports: [IonicModule],
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  imports: [IonicModule],
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent  implements OnInit {
