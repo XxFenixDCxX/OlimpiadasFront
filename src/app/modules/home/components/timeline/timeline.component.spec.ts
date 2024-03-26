01d188250d0377d5b4721a2f866e4247e4d61799
Javier <javierpe12345@gmail.com>
2024-03-26 12:25:55 +0100

    Añadir component nuevo dentro de home

---
+++ src/app/modules/home/components/timeline/timeline.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { TimelineComponent } from './timeline.component';
+
+describe('TimelineComponent', () => {
+  let component: TimelineComponent;
+  let fixture: ComponentFixture<TimelineComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TimelineComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TimelineComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { IonicModule } from '@ionic/angular';
+import { TimelineComponent } from './components/timeline/timeline.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, TimelineComponent],
   imports: [
     CommonModule, HomeRoutingModule, IonicModule
   ]
