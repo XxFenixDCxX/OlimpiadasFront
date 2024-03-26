71f34517a821b5214c921bcc4879908ca18a096b
Arkaitz Calvo Saldias <arkaitzcs@gmail.com>
2024-03-26 13:48:11 +0100

    implementado login de auth0

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAuth0 } from '@auth0/auth0-angular';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideAuth0({
-      domain: 'olimpiadas.eu.auth0.com',
-      clientId: '8pVwMYIOg4tpzic1dimziFK55wBs55LJ',
-      authorizationParams: {
-        redirect_uri: window.location.origin
-      }
-    }),
-  ]
-});
 
 if (environment.production) {
   enableProdMode();
