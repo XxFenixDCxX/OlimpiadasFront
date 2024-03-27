a911d9b7672f17708ae7eb262c6b18cb617e3c6e
Arkaitz Calvo Saldias <arkaitzcs@gmail.com>
2024-03-27 10:47:24 +0100

    Añadidas nuevas funciones de validación de formularios

--- src/app/modules/user-page/user-page.component.ts
+++ src/app/modules/user-page/user-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-user-page',
@@ -11,13 +12,17 @@ export class UserPageComponent implements OnInit {
   options: String[] = ["2323", "2323", "22323"];
   isAuthenticated: boolean = false; // Variable para almacenar el estado de autenticación
 
-  constructor(private auth: AuthService, private router: Router) { }
+  constructor(private auth: AuthService, private router: Router, private api:ApiService) { }
 
   ngOnInit(): void {
     this.auth.isAuthenticated$.subscribe((isAuthenticated: boolean) => {
       this.isAuthenticated = isAuthenticated;
       if (!this.isAuthenticated) {
         this.router.navigate(['/home']);
+      } else {
+        this.api.getAllUsers().subscribe((data: any) => {
+          console.log(data);
+        });
       }
     });
   }
