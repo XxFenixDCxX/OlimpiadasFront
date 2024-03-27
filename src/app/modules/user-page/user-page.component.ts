f222edd5dbe44d274723c0cb474ccb165ef50214
Arkaitz Calvo Saldias <arkaitzcs@gmail.com>
2024-03-27 10:43:19 +0100

    Actualizar componente de barra de navegación y página de usuario

--- src/app/modules/user-page/user-page.component.ts
+++ src/app/modules/user-page/user-page.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
+
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
-  styleUrls: ['./user-page.component.scss'],
+  styleUrls: ['./user-page.component.scss']
 })
 export class UserPageComponent implements OnInit {
-  options: String[] = ["2323", "2323", "22323"]
+  options: String[] = ["2323", "2323", "22323"];
+  isAuthenticated: boolean = false; // Variable para almacenar el estado de autenticación
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) { }
 
-  ngOnInit() { }
-
+  ngOnInit(): void {
+    this.auth.isAuthenticated$.subscribe((isAuthenticated: boolean) => {
+      this.isAuthenticated = isAuthenticated;
+      if (!this.isAuthenticated) {
+        this.router.navigate(['/home']);
+      }
+    });
+  }
 }
