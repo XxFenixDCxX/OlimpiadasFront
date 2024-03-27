475f592c73ca51356c0ebd3beb1e125f05254ef9
Javier <javierpe12345@gmail.com>
2024-03-27 10:21:52 +0100

    Solución bug login redirect a home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '',  redirectTo: 'home', pathMatch: 'full'},
+  {path: '',  redirectTo: 'userpage', pathMatch: 'full'},
   {path: 'home', loadChildren: () => import('./modules').then(m => m.HomeModule)},
   {path: 'userpage', loadChildren: () => import('./modules').then(m => m.UserPageModule)}
 ];
