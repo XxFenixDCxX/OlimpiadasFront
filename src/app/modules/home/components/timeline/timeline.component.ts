a0200da1ac888bf0c755b0ea2fc6feddc2f15671
Arkaitz Calvo Saldias <arkaitzcs@gmail.com>
2024-03-30 17:16:05 +0100

    Resolicion vista movil timeline, falta otra parte

--- src/app/modules/home/components/timeline/timeline.component.ts
+++ src/app/modules/home/components/timeline/timeline.component.ts
@@ -6,32 +6,45 @@ import { AuthService } from '@auth0/auth0-angular';
   templateUrl: './timeline.component.html',
   styleUrls: ['./timeline.component.scss'],
 })
-export class TimelineComponent  implements AfterViewInit {
+export class TimelineComponent implements AfterViewInit {
 
   constructor(private elementRef: ElementRef, public auth: AuthService) { }
 
   ngAfterViewInit(): void {
-    const timelineEvents = this.elementRef.nativeElement.querySelectorAll('.timeline__event');
-    const observerOptions = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.5 // El porcentaje del elemento que debe ser visible para disparar el evento
-    };
-
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          const index = entry.target.getAttribute('data-index');
-          entry.target.classList.add('visible');
-          observer.unobserve(entry.target);
-        }
+    if (this.isMobileDevice()) { // Verifica si es un dispositivo móvil
+      const timelineEvents = this.elementRef.nativeElement.querySelectorAll('.timeline__event');
+      timelineEvents.forEach((event: Element) => {
+        event.classList.add('visible'); // Muestra todos los elementos directamente
       });
-    }, observerOptions);
+    } else { // Si no es un dispositivo móvil, activa la animación de desplazamiento
+      const timelineEvents = this.elementRef.nativeElement.querySelectorAll('.timeline__event');
+      const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5 // El porcentaje del elemento que debe ser visible para disparar el evento
+      };
 
-    timelineEvents.forEach((event: Element) => {
-      observer.observe(event);
-    });
+      const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const index = entry.target.getAttribute('data-index');
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+          }
+        });
+      }, observerOptions);
+
+      timelineEvents.forEach((event: Element) => {
+        observer.observe(event);
+      });
+    }
   }
+
+  isMobileDevice(): boolean {
+    return window.innerWidth <= 768; // Define el ancho máximo para considerarlo como dispositivo móvil
+  }
+
+
   loginWithRedirect(): void {
     this.auth.loginWithRedirect();
   }
