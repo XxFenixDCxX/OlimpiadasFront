3d4103a2b5670f51f1370414576098721cfcc36d
Mohammed Choudna <choudnamohammed@gmail.com>
2024-04-23 13:07:52 +0200

    Refactor event-details component and user-page component

--- src/app/modules/user-page/components/event-details/event-details.component.ts
+++ src/app/modules/user-page/components/event-details/event-details.component.ts
@@ -2,19 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormControl } from '@angular/forms';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
-  standalone: true,
-  imports: [IonicModule,CommonModule],
   selector: 'app-event-details',
   templateUrl: './event-details.component.html',
   styleUrls: ['./event-details.component.scss'],
 })
-export class EventDetailsComponent  implements OnInit {
+export class EventDetailsComponent implements OnInit {
+  eventId: number = 2; 
+  eventSections: any[] = []; 
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
-  ngOnInit() {}
-  category = new FormControl('');
+  ngOnInit() {
+    this.loadEventData();
+    console.log("")
+  }
 
+  loadEventData() {
+    this.apiService.getEventSections(this.eventId).subscribe((sections: any[]) => {
+      this.eventSections = sections;
+    });
+  }
 }
