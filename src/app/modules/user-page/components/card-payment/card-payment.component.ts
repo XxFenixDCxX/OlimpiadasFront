b1021419a02fbd573fe00bfaea6513c115debef3
eimarramos <eimarramos.sacari@gmail.com>
2024-04-24 13:18:10 +0200

    fix: Loaders

--- src/app/modules/user-page/components/card-payment/card-payment.component.ts
+++ src/app/modules/user-page/components/card-payment/card-payment.component.ts
@@ -46,8 +46,6 @@ export class CardPaymentComponent{
       console.error('Error realizando la compra:', error);
       alert(`Error realizando la compra: ${error.message}`);
       this.spinnerService.isBusySetData(false);
-    }finally{
-      this.spinnerService.isBusySetData(false);
     }
   }
 }
